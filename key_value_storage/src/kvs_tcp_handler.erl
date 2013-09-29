cb8f58b3bf439929c4c922f574fff36249b9da97
Yura Zhloba <yzh44yzh@gmail.com>
2013-09-29 11:09:57 +0300

    use kvs api with tcp handler

--- key_value_storage/src/kvs_tcp_handler.erl
+++ key_value_storage/src/kvs_tcp_handler.erl
@@ -2,6 +2,7 @@
 -author('Yura Zhloba <yzh44yzh@gmail.com>').
 
 -export([start_link/4, init/3]).
+-include("kvs_types.hrl").
 
 -record(state, {
 	  listener :: pid(),
@@ -33,7 +34,8 @@ loop(#state{socket = Socket, transport = Transport} = State) ->
         read_data -> case read_package(State, []) of
                          closed -> self() ! close;
                          [] -> self() ! read_data;
-                         Data -> self() ! {send, Data}
+                         Data -> Reply = process_data(Data),
+                                 self() ! {send, Reply}
                      end,
                      loop(State);
         {send, Data} -> Transport:send(Socket, Data),
@@ -54,4 +56,50 @@ read_package(#state{transport = Transport, socket = Socket} = State, Accum) ->
         {error, timeout} -> lists:reverse(Accum);
         {error, closed} -> closed
     end.
-                                                   
+                                      
+
+-spec(process_data([byte()]) -> [byte()]).
+process_data(Data) ->
+    Res = case parse_query(Data) of
+              {create, Key, Value} -> kvs:create(Key, Value);
+              {read, Key} -> kvs:read(Key);
+              {update, Key, Value} -> kvs:update(Key, Value);
+              {delete, Key} -> kvs:delete(Key);
+              error -> "invalid query"
+          end,
+    io_lib:format("~p\n", [Res]).
+            
+
+-spec(parse_query([byte()]) -> {create, key(), value()} |
+                               {read, key()} |
+                               {update, key(), value()} |
+                               {delete, key()} |
+                               error).
+parse_query(Data) ->
+    Data1 = case lists:reverse(Data) of
+                [10, 13 | Rest] -> lists:reverse(Rest);
+                [13 | Rest] -> lists:reverse(Rest);
+                [10 | Rest] -> lists:reverse(Rest);
+                _ -> Data
+            end,
+    case string:tokens(Data1, " ") of
+        ["create" | Tokens] when length(Tokens) > 1 ->
+            [Key | Value] = Tokens,
+            {create, Key, string:join(Value, " ")};
+        ["read" | Tokens] when length(Tokens) > 0 ->
+            [Key | _] = Tokens,
+            {read, Key};
+        ["update" | Tokens] when length(Tokens) > 1 ->
+            [Key | Value] = Tokens,
+            {update, Key, string:join(Value, " ")};
+        ["delete" | Tokens] when length(Tokens) > 0 ->
+            [Key | _] = Tokens,
+            {delete, Key};
+        _ -> error
+    end.
+                
+            
+            
+            
+        
+    
