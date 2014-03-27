aca3f27b15124b6ff5d1425ec0a2d49362f65e78
Yura Zhloba <yzh44yzh@gmail.com>
2014-03-28 01:02:51 +0300

    connect to nodes

--- distributed_chat/src/dchat_app.erl
+++ distributed_chat/src/dchat_app.erl
@@ -5,6 +5,9 @@
 -export([start/0, start/1, start/2, stop/1]).
 -include("logger.hrl").
 
+
+%%% module API
+
 start() -> start(['8080']).
 
 start([Port0]) ->
@@ -24,8 +27,12 @@ start([Port0]) ->
 
     ok = application:start(dchat),
     ?INFO("dchat app started"),
+
+    connect_nodes(),
     ok.
 
+
+%%% application API
 
 start(_StartType, _StartArgs) ->
     dchat_sup:start_link().
@@ -39,6 +46,8 @@ stop(_State) ->
     ok.
 
 
+%%% inner functions
+
 routes()->
     [{'_',
       [
@@ -47,3 +56,17 @@ routes()->
        {"/[...]", cowboy_static, {dir, "priv/static", [{mimetypes, cow_mimetypes, all}]}},
        {'_', not_found_handler, []}
       ]}].
+
+
+connect_nodes() ->
+    {ok, Nodes0} = application:get_env(dchat, nodes),
+    Nodes = lists:filter(fun(Node) -> Node =/= node() end, Nodes0),
+    ?INFO("try to connect nodes ~p", [Nodes]),
+    Results = lists:map(fun(Node) ->
+                                net_kernel:connect_node(Node)
+                        end, Nodes),
+    case lists:member(true, Results) of
+        true -> ?INFO("has connections ~p", [nodes()]);
+        false -> ?INFO("no connections")
+    end,
+    ok.
